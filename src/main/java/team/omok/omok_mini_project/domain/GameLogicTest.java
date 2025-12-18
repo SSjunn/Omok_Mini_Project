23bbd9101eb04f364a54d29eb30715358fd80550
huni6711 <huni6711@naver.com>
2025-12-18 17:55:29 +0900

    feat: 오목 에러 발생 테스트 주석처리

--- src/main/java/team/omok/omok_mini_project/domain/GameLogicTest.java
+++ src/main/java/team/omok/omok_mini_project/domain/GameLogicTest.java
@@ -1,18 +1,18 @@
-package team.omok.omok_mini_project.domain;
-
-import team.omok.omok_mini_project.enums.Stone;
-
-public class GameLogicTest {
-
-    public static void main(String[] args) {
-        GameState state = new GameState();
-        OmokRule rule = new OmokRule();
-
-        // 게임 시작
-        state.startGame();
-
-        
-        // Test 시, 해당 파트 주석 풀어서 게산
+//package team.omok.omok_mini_project.domain;
+//
+//import team.omok.omok_mini_project.enums.Stone;
+//
+//public class GameLogicTest {
+//
+//    public static void main(String[] args) {
+//        GameState state = new GameState();
+//        OmokRule rule = new OmokRule();
+//
+//        // 게임 시작
+//        state.startGame();
+//
+//        
+//        // Test 시, 해당 파트 주석 풀어서 게산
         
         
 //        // Test 1 (일반적인 오목 테스트) --> 통과
@@ -56,5 +56,5 @@ public class GameLogicTest {
 //        //Test 4 (범위 밖 착수 에러 테스트) --> 통과
 //        System.out.println(rule.placeStone(state, 7, 7)); // BLACK
 //        System.out.println(rule.placeStone(state, 7, 17)); // WHITE
-    }
-}
+//    }
+//}
